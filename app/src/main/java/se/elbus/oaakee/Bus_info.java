f53168bde001641f809bfbed6b1a9d17ab996a16
Mikael <git@evinja.se>
2015-09-25 01:29:27 +0200

    Added bus_info clas

---
+++ app/src/main/java/se/elbus/oaakee/Bus_info.java
@@ -0,0 +1,13 @@
+package se.elbus.oaakee;
+
+/**
+ * Created by mike on 2015-09-24.
+ */
+public class Bus_info {
+    public String resourceSpec;
+    public String gatewayId;
+    public long timestamp;
+    public String value;
+
+
+}
