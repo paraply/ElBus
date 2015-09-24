62351423ef7328bc43e5d7ebb7a0acfd239eae45
cpupro <practical_master@hotmail.com>
2015-09-24 20:14:06 +0200

    Deleted MainActivity and its layout file.

--- app/src/main/java/se/elbus/oaakee/TravelActivity.java
+++ app/src/main/java/se/elbus/oaakee/TravelActivity.java
@@ -14,7 +14,7 @@ public class TravelActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_travel);
     }
 
     @Override
