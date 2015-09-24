a8059783df1b2b4cd9dcf58f915d5dd95880d1ad
Mikael <git@evinja.se>
2015-09-25 01:42:41 +0200

    Added Electricity API interface: Get sensors & resources from one/every bus

--- app/src/main/java/se/elbus/oaakee/ECity_Client.java
+++ app/src/main/java/se/elbus/oaakee/ECity_Client.java
@@ -1,9 +1,36 @@
 package se.elbus.oaakee;
 
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.http.GET;
+import retrofit.http.Header;
+import retrofit.http.Query;
+
 /**
  * Created by mike on 2015-09-23.
  */
 public class ECity_Client {
 
 
+    private interface ECity_API{
+
+        // Will request a GET like: ?dgw=Ericsson$Vin_Num_001&t1=1443100184000&t2=1443107384000&sensorSpec=Ericsson$GPS
+        @GET("ecity")
+        Call<List<Bus_info>> get_bus_sensor(@Header("Authorization") String authorization, @Query("dgw") String bus_ID, @Query("t1") String time1, @Query("t2") String time2, @Query("sensorSpec") String sensor );
+
+        //Bus ID omitted - get sensor for all buses
+        @GET("ecity")
+        Call<List<Bus_info>> get_all_buses_sensors(@Header("Authorization") String authorization, @Query("t1") String time1, @Query("t2") String time2, @Query("sensorSpec") String sensor );
+
+
+        //Will request a GET like: ?dgw=Ericsson$Vin_Num_001&t1=1443100184000&t2=1443107384000&resourceSpec=Ericsson$Latitude_Value
+        @GET("ecity")
+        Call<List<Bus_info>> get_bus_resource(@Header("Authorization") String authorization, @Query("dgw") String bus_ID, @Query("t1") String time1, @Query("t2") String time2, @Query("resourceSpec") String resource );
+
+        //Bus ID omitted - get resources for all buses
+        @GET("ecity")
+        Call<List<Bus_info>> get_all_buses_resources(@Header("Authorization") String authorization,  @Query("t1") String time1, @Query("t2") String time2, @Query("resourceSpec") String resource );
+
+    }
 }
