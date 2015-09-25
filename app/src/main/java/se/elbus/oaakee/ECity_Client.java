9a867ba66acff86594bb49f0d4eb22f99802ad54
Mikael <git@evinja.se>
2015-09-25 02:02:10 +0200

    Get_bus_sensor method added

--- app/src/main/java/se/elbus/oaakee/ECity_Client.java
+++ app/src/main/java/se/elbus/oaakee/ECity_Client.java
@@ -1,10 +1,13 @@
 package se.elbus.oaakee;
 
 import android.util.Base64;
+import android.util.Log;
 
+import java.util.Date;
 import java.util.List;
 
 import retrofit.Call;
+import retrofit.Callback;
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
 import retrofit.http.GET;
@@ -29,6 +32,36 @@ public class ECity_Client {
                     .build();
             ecity_api = client.create(ECity_API.class);
         }
+    }
+
+    public void get_bus_sensor(String bus_ID_or_empty, Date start_time, Date end_time, String sensor ){
+        Call<List<Bus_info>> call;
+        if (bus_ID_or_empty.isEmpty()){
+            call = ecity_api.get_all_buses_sensors(CREDENTIALS_BASE64, Long.toString(start_time.getTime()), Long.toString(end_time.getTime()), sensor);
+            Log.i("### get_all_buses_sens", " time1:" + Long.toString(start_time.getTime()) + " time2:" + Long.toString(end_time.getTime()));
+        }else{
+            call = ecity_api.get_bus_sensor(CREDENTIALS_BASE64, bus_ID_or_empty, Long.toString(start_time.getTime()), Long.toString(end_time.getTime()), sensor);
+            Log.i("### get_bus_sensor", "ID:" + bus_ID_or_empty + " time1:" + Long.toString(start_time.getTime()) + " time2:" + Long.toString(end_time.getTime()) );
+        }
+
+        call.enqueue(new Callback<List<Bus_info>>() {
+            @Override
+            public void onResponse(retrofit.Response<List<Bus_info>> response) {
+                if (response.isSuccess()) {
+                    List<Bus_info> result = response.body();
+                    for (Bus_info res : result){
+                        Log.i("### SNS RESULT", "BUS ID:" +  res.gatewayId + " RESOURCE:" + res.resourceSpec + " VALUE:" + res.value + " TIME:" + res.timestamp  );
+                    }
+                }else {
+                        Log.i("### ERR", "get_bus_sensor response.isSuccess returned false");
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.i("### ERR get_bus_sensor",t.getMessage());
+            }
+        });
     }
 
 
