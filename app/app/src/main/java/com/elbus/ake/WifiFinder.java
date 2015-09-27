2bb525dce172b698db423b6c9f789384b7d5988b
Thomas Högberg <blobber93@gmail.com>
2015-09-27 23:13:32 +0200

    Added class to handle the wifi finding

---
+++ app/app/src/main/java/com/elbus/ake/WifiFinder.java
@@ -0,0 +1,89 @@
+package com.elbus.ake;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+
+import com.elbus.ake.Buses.Bus;
+import com.elbus.ake.Buses.Buses;
+
+/**
+ * Created by TH on 2015-09-27.
+ */
+public abstract class WifiFinder extends BroadcastReceiver {
+
+    private final String wifiName;
+    private boolean wifiOff; // Assumed to be false at first.
+
+    protected WifiFinder(Context context, String wifiName) {
+        this.wifiName = wifiName;
+
+        WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+        if(wifi.setWifiEnabled(true)){
+            this.wifiOff = true;
+            wifi.startScan();
+        }
+
+    }
+
+    /**
+     * This method will take the received data and find the closest one that matches the defined search expression.
+     * If it can't find a result, it's going to retun NULL // TODO: Make this class NOT send NULL to handleData().
+     * @param context is the context to take the wifi from.
+     * @param intent // TODO: Add description.
+     */
+    @Override
+    public void onReceive(Context context, Intent intent){
+        context.unregisterReceiver(this);
+        WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+        ScanResult result = null;
+        for (ScanResult sr : wifi.getScanResults()){
+            /*
+             * If we don't have a result yet it will add it if matching the string,
+             * otherwise it's going to replace the result if we get a better signal.
+             * TODO: Add a better way of finding closest wifi.
+             */
+            if((sr.SSID.matches(this.wifiName) && (result == null)) ||
+                    ((result != null) && isCloser(sr, result))) {
+                result = sr;
+            }
+        }
+
+        if(this.wifiOff){
+            wifi.setWifiEnabled(false);
+        }
+        this.handleData(result);
+    }
+
+    /**
+     * If s1 had a higher or the same level, this will return true.
+     */
+    public static boolean isCloser(ScanResult s1, ScanResult s2){
+        return s1.level - s2.level >= 0;
+    }
+
+    /**
+     * Default implementation will try to find the dgw of the bus associated with the mac-adress.
+     * @param wifi is the closest wifi matching the search results.
+     */
+    public void handleData(ScanResult wifi){
+        if (wifi == null){
+            return;
+        }
+        String dgw = null;
+        Bus bus = Buses.findByMac(wifi.BSSID);
+        dgw = bus.getDgw();
+        receiveDgw(dgw);
+    }
+
+    /**
+     * This is needed to receive the dgw somewhere.
+     * @param dgw is the dgw of the wifi found.
+     */
+    public abstract void receiveDgw(String dgw);
+
+}
