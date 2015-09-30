4181111a8b6bb5e5e7b4a2d2aedd21712266ad1c
cpupro <practical_master@hotmail.com>
2015-09-30 15:33:08 +0200

    InfoActivity now starts the fragment. (InfoActivity is temporary and made just for this branch.)

--- app/src/main/java/se/elbus/oaakee/InfoActivity.java
+++ app/src/main/java/se/elbus/oaakee/InfoActivity.java
@@ -1,6 +1,8 @@
 package se.elbus.oaakee;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +13,14 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new InfoFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 
     @Override
