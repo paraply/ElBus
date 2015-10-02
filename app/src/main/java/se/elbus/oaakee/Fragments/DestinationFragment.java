611baa01cffac40ccaa5b2f48981248516164324
Tobias <toobew@users.noreply.github.com>
2015-10-02 09:05:18 +0200

    Moved to package. Created settings fragment

---
+++ app/src/main/java/se/elbus/oaakee/Fragments/DestinationFragment.java
@@ -0,0 +1,47 @@
+package se.elbus.oaakee.Fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import se.elbus.oaakee.R;
+
+/**
+ * Fragment for the "Choose destination"-layout
+ *
+ * Created by Tobias on 15-09-30.
+ */
+public class DestinationFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View destinationView =  inflater.inflate(R.layout.destination_chooser, container, false);
+
+        populateDestinationsList(destinationView);
+
+        return destinationView;
+    }
+
+    /**
+     * Populates destination list
+     *
+     * @param destinationView The choose destination view
+     */
+    private void populateDestinationsList(View destinationView){
+        String destinations[] = {"Kapellplatsen", "Götaplatsen", "Valand", "Kungsportsplatsen", "Domkyrkan", "Lilla Bommen"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),R.layout.destination_chooser,destinations);
+        ListView destinationsListView = (ListView) destinationView.findViewById(R.id.destinationsListView);
+        destinationsListView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/se/elbus/oaakee/Fragments/SettingsFragment.java
@@ -0,0 +1,21 @@
+package se.elbus.oaakee.Fragments;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tobias on 2015-10-02.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
