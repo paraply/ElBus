5d1b14526fa27d22b06c6b9310cc15e9509269d9
cpupro <practical_master@hotmail.com>
2015-10-03 19:44:34 +0200

    UI now changes slightly depending on state, simulate travel by pressing the button.

--- app/src/main/java/se/elbus/oaakee/InfoFragment.java
+++ app/src/main/java/se/elbus/oaakee/InfoFragment.java
@@ -7,9 +7,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class InfoFragment extends Fragment {
 
     private TextView mTV;
+    private boolean onBus;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,17 +23,23 @@ public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_info, container, false);
 
+        onBus = false;
+
+        Random ran = new Random();
 
         mTV = (TextView) v.findViewById(R.id.timeTilArrival);
-        mTV.setText("17");
+        mTV.setText(Integer.toString(ran.nextInt(13) + 2));
         mTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View vClick) {
-                if (mTV.getText().equals("17")) {
-                    //mTV.setText("7");
+                int minLeft = Integer.parseInt(mTV.getText().toString());
+                if (minLeft <= 0) {
+                    onBus = !onBus;
+                    Random ran = new Random();
+                    mTV.setText(Integer.toString(ran.nextInt(13) + 2));
                     modifyView(InfoFragment.this.getView());
                 } else {
-                    mTV.setText("17");
+                    mTV.setText(Integer.toString(minLeft - 1));
                 }
             }
         });
@@ -39,8 +48,15 @@ public class InfoFragment extends Fragment {
     }
 
     private void modifyView(View v) {
-        mTV = (TextView) v.findViewById(R.id.timeTilArrival);
-        mTV.setText("8");
-
+        TextView tView = (TextView) v.findViewById(R.id.infoArrivesIn);
+        TextView tBusName = (TextView) v.findViewById(R.id.infoBusName);
+        if(onBus) {
+            tView.setText("framme om");
+        } else {
+            Random ran = new Random();
+            int busNr = ran.nextInt(55) + 1;
+            tBusName.setText(busNr + ":an");
+            tView.setText("ankommer om");
+        }
     }
 }
