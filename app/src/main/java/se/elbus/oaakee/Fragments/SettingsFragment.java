7957d118342424d9586f2b24bdf8a54e2f643279
Tobias <toobew@users.noreply.github.com>
2015-10-07 13:23:54 +0200

    Removed imports

--- app/src/main/java/se/elbus/oaakee/Fragments/SettingsFragment.java
+++ app/src/main/java/se/elbus/oaakee/Fragments/SettingsFragment.java
@@ -2,15 +2,11 @@ package se.elbus.oaakee.Fragments;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import se.elbus.oaakee.R;
 
 /**
+ * Fragment to for Settings.
+ *
  * Created by Tobias on 2015-10-02.
  */
 public class SettingsFragment extends PreferenceFragment {
