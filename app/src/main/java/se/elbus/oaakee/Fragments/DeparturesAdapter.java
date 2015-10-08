ed042d4eeebea7f67cc340ddf5140115a74ba380
Tobias <toobew@users.noreply.github.com>
2015-10-08 18:05:08 +0200

    Now created buttons from a List of departures

--- app/src/main/java/se/elbus/oaakee/Fragments/DeparturesAdapter.java
+++ app/src/main/java/se/elbus/oaakee/Fragments/DeparturesAdapter.java
@@ -1,39 +1,80 @@
 package se.elbus.oaakee.Fragments;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import se.elbus.oaakee.R;
+import se.elbus.oaakee.REST_API.VT_Model.Departure;
 
 /**
- * Custom adapter for departures ListView. <p> TODO: should extend "ArrayAdapter<BusLine>" or
- * something similar instead of "<String>" <p> Created by Tobias on 15-09-27.
+ * Custom adapter for departures ListView.
+ *
+ * Created by Tobias on 15-09-27.
  */
-public class DeparturesAdapter extends ArrayAdapter<String> {
-    public DeparturesAdapter(Context context, String[] string) {
-        super(context, R.layout.busline_row, string);
+public class DeparturesAdapter extends ArrayAdapter<List<Departure>> {
+    public DeparturesAdapter(Context context, List<List<Departure>> departures) {
+        super(context, R.layout.busline_row, departures);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        Log.i("Travel","--- DeparturesAdapter getView ---");
         LayoutInflater layoutInflater = LayoutInflater.from(getContext());
         View customView = layoutInflater.inflate(R.layout.busline_row, parent, false);
 
-        String busDirection = getItem(position);
+        List<Departure> departures = getItem(position);
 
         final Button topButton = (Button) customView.findViewById(R.id.topDepartureButton);
         final Button bottomButton = (Button) customView.findViewById(R.id.bottomDepartureButton);
 
-        topButton.setText(busDirection);
-        bottomButton.setText(busDirection + " 2");
+        Button thirdButton = new Button(getContext());
+
+        Log.i("Travel","departure size: " + departures.size());
+
+        switch (departures.size()){
+            case 1:
+                topButton.setText(departures.get(0).name);
+                break;
+            case 2:
+                topButton.setText(departures.get(0).name);
+                bottomButton.setText(departures.get(1).name);
+                break;
+            case 3:
+                topButton.setText(departures.get(0).name);
+                bottomButton.setText(departures.get(1).name);
+                thirdButton.setText(departures.get(2).name);
+                break;
+            case 4:
+                topButton.setText("4");
+                bottomButton.setText("4");
+                thirdButton.setText("4");
+                break;
+            default:
+                topButton.setText("size " + departures.size());
+                bottomButton.setText("default");
+                thirdButton.setText("default");
+                break;
+        }
+
+        LinearLayout ll = (LinearLayout)customView.findViewById(R.id.busLineButtonLayout);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        ll.addView(thirdButton, lp);
+
+
 
         setButtonAction(topButton);
         setButtonAction(bottomButton);
+        setButtonAction(thirdButton);
 
         return customView;
     }
