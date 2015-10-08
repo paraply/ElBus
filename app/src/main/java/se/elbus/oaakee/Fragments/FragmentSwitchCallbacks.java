2c31f830355405345d32911317abce8f7c12001e
Thomas Högberg <blobber93@gmail.com>
2015-10-08 16:55:48 +0200

    Made new interface for fragment switching

---
+++ app/src/main/java/se/elbus/oaakee/Fragments/FragmentSwitchCallbacks.java
@@ -0,0 +1,11 @@
+package se.elbus.oaakee.Fragments;
+
+import android.os.Bundle;
+
+/**
+ * Created by gheez on 2015-10-08.
+ */
+public interface FragmentSwitchCallbacks {
+    void nextFragment(Bundle bundle);
+    void previousFragment(Bundle bundle);
+}
