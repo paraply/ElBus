8ea8a2a4e44bde26c0ccf84a2cec47e32a5afd2b
cpupro <practical_master@hotmail.com>
2015-10-09 19:27:21 +0200

    DestinationFragment now implements VT_Callback

--- app/src/main/java/se/elbus/oaakee/Fragments/DestinationFragment.java
+++ app/src/main/java/se/elbus/oaakee/Fragments/DestinationFragment.java
@@ -9,39 +9,71 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.List;
+
 import se.elbus.oaakee.R;
+import se.elbus.oaakee.REST_API.VT_Callback;
+import se.elbus.oaakee.REST_API.VT_Client;
+import se.elbus.oaakee.REST_API.VT_Model.DepartureBoard;
+import se.elbus.oaakee.REST_API.VT_Model.JourneyDetail;
+import se.elbus.oaakee.REST_API.VT_Model.LocationList;
 
 /**
  * Fragment for the "Choose destination"-layout
  *
  * Created by Tobias on 15-09-30.
  */
-public class DestinationFragment extends Fragment {
+public class DestinationFragment extends Fragment implements VT_Callback {
+
+    private ListView mDestinationsListView;
+    private VT_Client vtClient;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        vtClient = new VT_Client(this);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View destinationView =  inflater.inflate(R.layout.destination_chooser, container, false);
+        View v =  inflater.inflate(R.layout.destination_chooser, container, false);
 
-        populateDestinationsList(destinationView);
+        mDestinationsListView = (ListView) v.findViewById(R.id.destinationsListView);
 
-        return destinationView;
+        populateDestinationsList();
+
+        return v;
     }
 
     /**
      * Populates destination list
      *
-     * @param destinationView The choose destination view
      */
-    private void populateDestinationsList(View destinationView){
+    private void populateDestinationsList(){
         String destinations[] = {"Kapellplatsen", "Götaplatsen", "Valand", "Kungsportsplatsen", "Domkyrkan", "Lilla Bommen"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,destinations);
-        ListView destinationsListView = (ListView) destinationView.findViewById(R.id.destinationsListView);
-        destinationsListView.setAdapter(adapter);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,destinations);
+        mDestinationsListView.setAdapter(adapter);
+    }
+
+    @Override
+    public void got_journey_details(JourneyDetail journeyDetail) {
+
+    }
+
+    @Override
+    public void got_nearby_stops(LocationList locationList) {
+
+    }
+
+    @Override
+    public void got_departure_board(DepartureBoard departureBoard) {
+
+    }
+
+    @Override
+    public void got_error(String during_method, String error_msg) {
+
     }
 }
