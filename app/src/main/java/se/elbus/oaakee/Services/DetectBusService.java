6db15aed6b864642c3ba940ab8180639a68cb709
Anton Josefsson <gusjosanl@student.gu.se>
2015-10-12 10:31:41 +0200

    not even sure what I changed lol

--- app/src/main/java/se/elbus/oaakee/Services/DetectBusService.java
+++ app/src/main/java/se/elbus/oaakee/Services/DetectBusService.java
@@ -10,15 +10,21 @@ import android.util.Log;
 
 import java.util.Calendar;
 
+import se.elbus.oaakee.Buses.WifiFinder;
+import se.elbus.oaakee.R;
+
 /**
  * Created by Anton on 2015-10-11.
  */
 public class DetectBusService extends IntentService {
 
-    private static final String TAG = "AlarmService";
+    private static final String TAG = "DetectBusService";
 
     //Minimum interval from 5.1 is 60 seconds, this will be rounded up
     private static final int POLL_INTERVAL = 1000 * 30;
+
+    public static String dgwFound;
+    public static boolean onBus;
 
     public static Intent newIntent(Context context) {
         return new Intent(context, AlarmService.class);
@@ -51,5 +57,14 @@ public class DetectBusService extends IntentService {
         calendar.add(Calendar.MILLISECOND, POLL_INTERVAL);
 
         //Check wififinder for connection
+        WifiFinder wifiFinder = new WifiFinder(this, this.getString(R.string.buswifiname)) {
+            @Override
+            // Found an Dgw close to us. We assume we are on this bus
+            public void receiveDgw(String dgw) {
+                Log.i(TAG, "Found Wifi, dgw: " + dgw);
+                dgwFound = dgw;
+                onBus = true;
+            }
+        };
     }
 }
